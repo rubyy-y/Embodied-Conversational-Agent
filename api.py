0b03a84e11b577782d73d7431ba503b147ac6465
rubyy-y <rubinawaldbauer@gmail.com>
2022-12-14 11:03:55 +0100

    get user specific data and get statistical data functions

--- api.py
+++ api.py
@@ -11,109 +11,116 @@ def exists(username):
     else:
         return "This user does not exist."
 
+
 def user_stats(username):
     url = f"https://fortnite-api.com/v2/stats/br/v2?name={username}"
     quest = json.loads(requests.get(url, headers={'Authorization': key}).content)
     return quest
 
 
-#### DATA EXPLORATION ####
+def get_user_data(username, info):
+    """
+    returns playerspecific information
+    username: name of user
+    info: desired information
+    """
+
+    # check if user exists
+    if exists(username) == "This user does not exist.":
+        raise KeyError("This user does not exist.")
+    
+    # if it does, check if info key available
+    else:
+        data = user_stats(username)['data']
+        if info not in ['id', 'name', 'level', 'progress']:
+            raise KeyError("Enter valid info key: ['id', 'name', 'level', 'progress']")
+
+        elif info == 'id':
+            return data['account']['id']
+        elif info == 'name':
+            return data['account']['name']
+        elif info == 'level':
+            return data['battlePass']['level']
+        elif info == 'progress':
+            return data['battlePass']['progress']
+
+
+def get_stats(username, data, stats_key='all', type='solo'):
+    """
+    method returns given statistic for username
+    username: name of user
+    stats_key: ['all', 'keyboardMouse', 'gamepad', 'touch'] // def. 'all'
+    type: ['overall', 'solo', 'duo', 'trio', 'squad', 'ltm'] // def. 'solo'
+    data: type of statistic
+
+    """
+    # check if user exists
+    if exists(username) == "This user does not exist.":
+        raise KeyError("This user does not exist.")
+    
+    # if it does, check if stats_key available
+    else:
+        stats = user_stats(username)['data']['stats']
+        if not stats_key in list(stats):
+            raise KeyError("Enter valid stats_key: ['all', 'keyboardMouse', 'gamepad', 'touch']")
+        
+        # if it is, check if type of data is available
+        else:
+            if not type in list(stats[stats_key]):
+                raise KeyError("Enter valid type: ['overall', 'solo', 'duo', 'trio', 'squad', 'ltm']")
+            
+            # if it is, check if data available
+            if not data in list(stats[stats_key][type]):
+                raise KeyError("Enter valid type of data.")
+            
+            # if it is, return data
+            else:
+                # print(stats[stats_key][type][data])
+                return stats[stats_key][type][data]
+
+
+
+# __________________________________________________________________________________________
+#### DATA STRUCTURE ####
 
 # Structure of aquired dict object:
 # user (dict): ['status', 'data']
-    # status (int): connection status
+    # // status (int): connection status
     # data (dict): ['account', 'battlePass', 'image', 'stats']
         # account (dict): ['id', 'name']
-            # id (str): userid
-            # name (str): username
+            # // id (str): userid
+            # // name (str): username
         # battlePass (dict): ['level', 'progress']
-            # level (int): current level
-            # progress (int): current progress
+            # // level (int): current level
+            # // progress (int): current progress
         # image (NoneType)
         # stats (dict): ['all', 'keyboardMouse', 'gamepad', 'touch']
             # all (dict): ['overall', 'solo', 'duo', 'trio', 'squad', 'ltm']
             # keyboardMouse (dict): ['overall', 'solo', 'duo', 'trio', 'squad', 'ltm']
             # gamepad (dict): ['overall', 'solo', 'duo', 'trio', 'squad', 'ltm']
             # touch (dict): ['overall', 'solo', 'duo', 'trio', 'squad', 'ltm']
-                # >>> overall (dict): ['score', 'scorePerMin', 'scorePerMatch', 'wins', 'top3', 'top5', 'top6', 'top10', 'top12', 'top25', 'kills', 'killsPerMin', 'killsPerMatch', 'deaths', 'kd', 'matches', 'winRate', 'minutesPlayed', 'playersOutlived', 'lastModified']
-                # >>> solo (dict):    ['score', 'scorePerMin', 'scorePerMatch', 'wins', 'top10', 'top25', 'kills', 'killsPerMin', 'killsPerMatch', 'deaths', 'kd', 'matches', 'winRate', 'minutesPlayed', 'playersOutlived', 'lastModified']
-                # >>> duo (dict):     ['score', 'scorePerMin', 'scorePerMatch', 'wins', 'top5', 'top12', 'kills', 'killsPerMin', 'killsPerMatch', 'deaths', 'kd', 'matches', 'winRate', 'minutesPlayed', 'playersOutlived', 'lastModified']
-                # >>> trio (NoneType)
-                # >>> squad (dict):   ['score', 'scorePerMin', 'scorePerMatch', 'wins', 'top3', 'top6', 'kills', 'killsPerMin', 'killsPerMatch', 'deaths', 'kd', 'matches', 'winRate', 'minutesPlayed', 'playersOutlived', 'lastModified']
-                # >>> ltm (dict):     ['score', 'scorePerMin', 'scorePerMatch', 'wins', 'kills', 'killsPerMin', 'killsPerMatch', 'deaths', 'kd', 'matches', 'winRate', 'minutesPlayed', 'playersOutlived', 'lastModified']
-
-# status = us['status']
-# data = us['data']
-
-# account = data['account']
-# battlePass = data['battlePass']
-# image = data['image']
-# stats = data['stats']
-
-# id = account['id']
-# name = account['name']
-# level = battlePass['level']
-# progress = battlePass['progress']
-# all = stats['all']
-# keyboardMouse = stats['keyboardMouse']
-# gamepad = stats['gamepad']
-# touch = stats['touch']
-
-# input = input('Input user name: ')
-input = 'p-six'
-
-if not exists(input):
-    raise KeyError('User does not Exist!')
-else:
-    us = user_stats(username=input)
-
-
-def get_data(key):
-    data = us['data']
-    account = data['account']
-    battlePass = data['battlePass']
-
-    id = account['id']
-    name = account['name']
-    level = battlePass['level']
-    progress = battlePass['progress']
-
-    if key == 'id':
-        return id
-    elif key == 'name':
-        return name
-    elif key == 'level':
-        return level
-    elif key == 'progress':
-        return progress
-    else:
-        return "enter valid key: 'id', 'name', 'level', 'progress'"
-
-def get_stats(method, mode):
-    data = us['data']
-    stats = data['stats']
-
-    if method == 'all':
-        key = stats['all']
-    elif method == 'keyboardMouse':
-        key = stats['keyboardMouse']
-    elif method == 'gamepad':
-        key = stats['gamepad']
-    elif method == 'touch':
-        key = stats['touch']
-    else:
-        return "enter valid method: 'all', 'keyboardMouse', 'gamepad', 'touch'"
-
-    if mode == 'overall':
-        return key['overall']
-    elif mode == 'solo':
-        return key['solo']
-    elif mode == 'duo':
-        return key['duo']
-    elif mode == 'squad':
-        return key['squad']
-    elif mode == 'ltm':
-        return key['ltm']
-    else:
-        return "enter valid mode: 'overall', 'solo', 'duo', 'squad', 'ltm'"
-
+                # >>> overall (dict): ['score', 'scorePerMin', 'scorePerMatch', 'wins', 
+                #                      'top3', 'top5', 'top6', 'top10', 'top12', 'top25', 
+                #                      'kills', 'killsPerMin', 'killsPerMatch', 'deaths', 
+                #                      'kd', 'matches', 'winRate', 'minutesPlayed', 
+                #                      'playersOutlived', 'lastModified']
+                # >>> solo (dict): ['score', 'scorePerMin', 'scorePerMatch', 'wins', 
+                #                   'top10', 'top25', 
+                #                   'kills', 'killsPerMin', 'killsPerMatch', 'deaths', 
+                #                   'kd', 'matches', 'winRate', 'minutesPlayed', 
+                #                   'playersOutlived', 'lastModified']
+                # >>> duo (dict): ['score', 'scorePerMin', 'scorePerMatch', 'wins', 
+                #                  'top5', 'top12', 
+                #                  'kills', 'killsPerMin', 'killsPerMatch', 'deaths', 
+                #                  'kd', 'matches', 'winRate', 'minutesPlayed', 
+                #                  'playersOutlived', 'lastModified']
+                # >>> trio (NoneType) ... explanation on api website
+                # >>> squad (dict): ['score', 'scorePerMin', 'scorePerMatch', 'wins', 
+                #                    'top3', 'top6',
+                #                    'kills', 'killsPerMin', 'killsPerMatch', 'deaths', 
+                #                    'kd', 'matches', 'winRate', 'minutesPlayed', 
+                #                    'playersOutlived', 'lastModified']
+                # >>> ltm (dict):     ['score', 'scorePerMin', 'scorePerMatch', 'wins', 
+                #                      'kills', 'killsPerMin', 'killsPerMatch', 'deaths',
+                #                      'kd', 'matches', 'winRate', 'minutesPlayed', 
+                #                      'playersOutlived', 'lastModified']
